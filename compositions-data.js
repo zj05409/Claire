b9e1738087b009a9d01639c173a9c300665e95d4
zj05409 <zj05409@gmail.com>
2026-06-12 18:36:24 +0800

    Delete composition: 我的未来神衣

--- compositions-data.js
+++ compositions-data.js
@@ -1,10 +1,2 @@
-window.CLAIRE_COMPOSITIONS_VERSION = "20260612183612";
-window.CLAIRE_COMPOSITIONS = [
-  {
-    "slug": "my-future-magic-clothes",
-    "title": "我的未来神衣",
-    "date": "2026-06-11",
-    "summary": "我设计了一件能防污、变换图案、自动调温的未来衣服。",
-    "image": "assets/compositions/my-future-magic-clothes.jpg"
-  }
-];
+window.CLAIRE_COMPOSITIONS_VERSION = "20260612183624";
+window.CLAIRE_COMPOSITIONS = [];
