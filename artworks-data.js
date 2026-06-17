12e92ba8d68cc8533139836b511ab5f8b02303ef
zj05409 <zj05409@gmail.com>
2026-06-17 13:28:57 +0800

    Publish artwork: 素描基本技法20260327

--- artworks-data.js
+++ artworks-data.js
@@ -1,2 +1,10 @@
-window.CLAIRE_ARTWORKS_VERSION = "20260612-initial";
-window.CLAIRE_ARTWORKS = [];
+window.CLAIRE_ARTWORKS_VERSION = "20260617132857";
+window.CLAIRE_ARTWORKS = [
+  {
+    "slug": "20260327",
+    "title": "素描基本技法20260327",
+    "date": "2026-06-17",
+    "summary": "Claire 的画作。",
+    "image": "assets/artworks/20260327.jpg"
+  }
+];
