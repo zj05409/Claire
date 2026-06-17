fbdffe1ddfb421316f4ebb0b34b69eab51f64e6e
zj05409 <zj05409@gmail.com>
2026-06-17 13:44:53 +0800

    Delete artwork: 素描基本技法20260327

--- artworks-data.js
+++ artworks-data.js
@@ -1,10 +1,2 @@
-window.CLAIRE_ARTWORKS_VERSION = "20260617132857";
-window.CLAIRE_ARTWORKS = [
-  {
-    "slug": "20260327",
-    "title": "素描基本技法20260327",
-    "date": "2026-06-17",
-    "summary": "Claire 的画作。",
-    "image": "assets/artworks/20260327.jpg"
-  }
-];
+window.CLAIRE_ARTWORKS_VERSION = "20260617134453";
+window.CLAIRE_ARTWORKS = [];
