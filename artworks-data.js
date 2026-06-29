e61e65d9506c4d63896b33ef502afa70f8a46f94
zj05409 <zj05409@gmail.com>
2026-06-29 19:15:37 +0800

    Publish artwork: 弯月上的星夜甜梦

--- artworks-data.js
+++ artworks-data.js
@@ -1,5 +1,12 @@
-window.CLAIRE_ARTWORKS_VERSION = "20260617155439";
+window.CLAIRE_ARTWORKS_VERSION = "20260629191537";
 window.CLAIRE_ARTWORKS = [
+  {
+    "slug": "artwork-2026-06-29-87ec1b63",
+    "title": "弯月上的星夜甜梦",
+    "date": "2026-06-29",
+    "summary": "蓝调星空撒满亮晶晶的碎星，红头发小姑娘蜷在暖黄色弯月上，笑意软融融的，拥抱着整片温柔梦幻的夜色。",
+    "image": "assets/artworks/artwork-2026-06-29-87ec1b63.jpg"
+  },
   {
     "slug": "artwork-2026-06-17-d2f2175b",
     "title": "素描基础认知练习",
